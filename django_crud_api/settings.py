7487db10557e95c551f7be6441aa2130850beb9c
herbertdm <shdelgado.m@gmail.com>
2023-06-22 15:28:37 -0400

    modificacion STATIC_URL

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 if not DEBUG:   
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
