5ff794acd48b7655259c28b835219761ccd4ca2e
herbertdm <shdelgado.m@gmail.com>
2023-06-23 10:59:08 -0400

    quitar client de repositorio

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -129,8 +129,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 if not DEBUG:   
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
     
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
